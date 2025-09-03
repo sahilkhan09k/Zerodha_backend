ceb428954635cdffb30bee3b985f20d82c92c3f5
sahilkhan09k <sahilkhan392005@gmail.com>
2025-09-03 22:58:00 +0530

    cors fixes

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({
 }));
 
 app.use(cors({
-    origin: ['https://zerodha-frontend-phi.vercel.app/', 'https://zerodha-dashboard-gilt.vercel.app/'],
+    origin: ['https://zerodha-frontend-phi.vercel.app', 'https://zerodha-dashboard-gilt.vercel.app'],
     credentials: true
 }))
 
